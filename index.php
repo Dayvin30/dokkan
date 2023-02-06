a4db13028826cfc62786fcd860c6a22d3c112b15
Dayvin30 <92032342+Dayvin30@users.noreply.github.com>
2023-02-06 15:06:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <head>
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/css/toastr.css" rel="stylesheet"/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
