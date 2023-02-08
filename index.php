433a4946a77dace77a36249a897d048d88d023a6
Dayvin30 <92032342+Dayvin30@users.noreply.github.com>
2023-02-08 09:44:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/css/toastr.css" rel="stylesheet"/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="script.js"></script>
     <link href="style.css" rel="stylesheet">
