fd945f4e06b53654f4be1af6a6b1c45ded4f2482
Dayvin30 <dayvin30@gmail.com>
2024-09-29 16:03:47 +0200

    ok

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
     <script type="text/javascript"  src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script type="text/javascript" src="script/script.js"></script>
-    <link href="style/style.css" rel="stylesheet">
+    <link href="../style/style.css" rel="stylesheet">
    <?php function getCategory()
 {
     $category =
